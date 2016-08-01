3aaa3de9a6eb8d6cdb960e5ef5c0447d8433cad9
Whittefang <whittefang@gmail.com>
2016-07-31 21:40:07 -0500

    movment changes

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class PlayerMovementScript : MonoBehaviour {
 	Rigidbody2D RB;
-	public float speedMax = 0;
+	public float speedMax = 0, jumpForce = 0, jumpDistance = 0;
 	float currentSpeed = 0;
 	InputScript IS;
 	float deadSize = .15f;
-	bool canMove = true;
+	bool canMove = true, grounded = true;
+	int groundedBuffer = 0;
+	int groundMask = 1 << 8;
 	// Use this for initialization
 	void Awake () {
 		IS = GetComponent<InputScript> ();
@@ -15,7 +17,7 @@ public class PlayerMovementScript : MonoBehaviour {
 		IS.SetThumbstick (ProcessMovement);
 		ResetSpeed ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
@@ -28,7 +30,11 @@ public class PlayerMovementScript : MonoBehaviour {
 			if (y < deadSize && y > -deadSize) {
 				y = 0;
 			}
-			RB.velocity = new Vector2 (x * currentSpeed, y * currentSpeed);
+			GroundCheck ();
+			JumpCheck (x, y);
+			if (grounded) {
+				RB.velocity = new Vector2 (x * currentSpeed, RB.velocity.y);
+			}
 		}
 	}
 	public void SetSpeed(float newSpeed = 0){
@@ -37,18 +43,66 @@ public class PlayerMovementScript : MonoBehaviour {
 	public void ResetSpeed(){
 		currentSpeed = speedMax;
 	}
-	public void Charge(float speed,Vector2 direction, float duration){
-		StartCoroutine (ChargeNumer (speed, direction, duration));
-	}
-	IEnumerator ChargeNumer(float speed, Vector2 direction, float duration){
-		canMove = false;
-		RB.velocity = direction * speed;
-		while (duration > 0){
-			yield return null;
-			duration -= .0167f;
+	public void JumpCheck(float x, float y){
+		if (y > .25f) {
+			if (x > .25f) {
+				// forward
+				TowardJump();
 
+			} else if (x < -.25f) {
+				// back
+				AwayJump();
+			} else {
+				// neutral
+				NeutralJump();
+			} 
 		}
-		canMove = true;
-		
 	}
+	public void NeutralJump(){
+		if (grounded) {
+
+			// prejump frames
+			RB.AddForce (new Vector2 (0, jumpForce));
+			grounded = false;
+			groundedBuffer = 3;
+		}
+	}
+	public void TowardJump(){
+		if (grounded){
+
+			// prejump frames
+			RB.AddForce (new Vector2(jumpDistance, jumpForce));
+			grounded = false;
+			groundedBuffer = 3;
+		}
+	}
+	public void AwayJump(){
+		if (grounded) {
+			// prejump frames
+			RB.AddForce (new Vector2 (-jumpDistance, jumpForce));
+			grounded = false;
+			groundedBuffer = 3;
+		}
+	}
+	public void GroundCheck(){
+		if (!grounded) {
+			// do a check for ground
+			//.35f
+			Debug.Log("beep");
+			RaycastHit2D groundCheck = Physics2D.Raycast (transform.position, Vector2.down, .7f, groundMask);
+
+			if (groundCheck.collider != null && groundedBuffer <= 0) {
+
+				// landing frames
+
+				Debug.Log("biip");
+				grounded = true;
+			} else if (groundedBuffer > 0) {
+				groundedBuffer--;
+
+				Debug.Log("boop");
+			}
+		}
+	}
+
 }
