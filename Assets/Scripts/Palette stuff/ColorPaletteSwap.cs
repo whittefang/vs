24994f5ed8fbab6f62de179df03966c5f2decb46
Whittefang <whittefang@gmail.com>
2016-08-31 11:04:20 -0500

    more palette impl

--- Assets/Scripts/Palette stuff/ColorPaletteSwap.cs
+++ Assets/Scripts/Palette stuff/ColorPaletteSwap.cs
@@ -6,26 +6,17 @@ public class ColorPaletteSwap : MonoBehaviour {
 
 	public Texture2D originalPalette;
 	public Texture2D newPalette;
+	public int uniqueColors = 0;
+	public List<Color32> foundColors = new List<Color32>();
+	public List<Color32> foundColorsnew = new List<Color32>();
 	// Use this for initialization
 	void Start () {
-		int uniqueColors = 0;
-		List<Color32> foundColors = new List<Color32>();
+
+		// move original into a texture
+
 		foreach (Color32 found in originalPalette.GetPixels()) {
-			bool match = false;
-			//Debug.Log (found);
-
-			foreach (Color32 existing in foundColors) {
-				if (found.r == existing.r && found.g == existing.g && found.b == existing.b) {// && found.a == existing.a) {
-					match = true;
-					break;
-				}
-
-			}
-			if (!match) {
 				uniqueColors++;
 				foundColors.Add (found);
-				//Debug.Log (found);
-			}
 		}
 		Texture2D generatedPallete;
 		generatedPallete = new Texture2D (uniqueColors, 1, TextureFormat.ARGB32, false, false);
@@ -33,12 +24,31 @@ public class ColorPaletteSwap : MonoBehaviour {
 		for (int x = 0; x < uniqueColors; x++) {
 			generatedPallete.SetPixel (x, 0, foundColors [x]);
 		}
+		Debug.Log (uniqueColors);
 		generatedPallete.Apply ();
+	
+
+
+		uniqueColors = 0;
+		foreach (Color32 found in newPalette.GetPixels()) {
+			uniqueColors++;
+			foundColorsnew.Add (found);
+		}
+		Texture2D NewPaletteGeneration;
+		NewPaletteGeneration = new Texture2D (uniqueColors, 1, TextureFormat.ARGB32, false, false);
+		NewPaletteGeneration.filterMode = FilterMode.Point;
+		for (int x = 0; x < uniqueColors; x++) {
+			NewPaletteGeneration.SetPixel (x, 0, foundColorsnew [x]);
+		}
+		Debug.Log (uniqueColors);
+		NewPaletteGeneration.Apply ();
+
+
 
 		SpriteRenderer targetSprite = GetComponent<SpriteRenderer> ();
 		targetSprite.material.SetTexture("_ColorPaletteOriginal", generatedPallete);
-		targetSprite.material.SetTexture("_ColorPaletteMorph", newPalette);
-		targetSprite.material.SetInt ("_textureWidth", uniqueColors +1);
+		targetSprite.material.SetTexture("_ColorPaletteMorph", NewPaletteGeneration);
+		targetSprite.material.SetInt ("_textureWidth", uniqueColors +1 );
 	}
 	
 	// Update is called once per frame
